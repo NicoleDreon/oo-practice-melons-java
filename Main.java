1c41c9198e969cd25135055eacb974628a44c3c1
Nicole Dreon <dreon.nicole@gmail.com>
2021-06-03 18:00:08 +0000

    add new melons of MelonType and add melons to MelonType array

--- Main.java
+++ Main.java
@@ -2,6 +2,46 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Instantiate objects here!");
-	}
 
+			MelonType musk = new MelonType(
+		"musk",
+		"Muskmelon",
+		1998,
+		"green",
+		true,
+		true
+	);
+
+	MelonType casaba = new MelonType(
+		"cas",
+		"Casaba",
+		2003,
+		"orange",
+		true,
+		false
+	);
+
+	MelonType crenshaw = new MelonType(
+		"cren",
+		"Crenshaw",
+		1996,
+		"green",
+		true,
+		false
+	);
+
+	MelonType yellowWatermelon = new MelonType(
+		"yw",
+		"Yellow Watermelon",
+		2013,
+		"yellow",
+		true,
+		false
+	);
+
+	MelonType[] melonTypes = {musk, casaba, crenshaw, yellowWatermelon};
+	
+	// System.out.println(Arrays.toString(melonTypes));
+	
+	}
 }
